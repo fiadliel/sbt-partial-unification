9ce442c02203ae927471466a5d17125effd56b25
Gary Coady <gary@lyranthe.org>
2016-05-27 14:23:50 +0100

    Update to fix scala version parsing.

--- src/main/scala/PartialUnification.scala
+++ src/main/scala/PartialUnification.scala
@@ -14,23 +14,25 @@ object PartialUnification extends AutoPlugin {
 
   def scalaVersionToDependencies(scalaVersion: String, partialUnificationModule: ModuleID): Seq[ModuleID] = {
     val pluginDependency = compilerPlugin(partialUnificationModule cross CrossVersion.full)
+
     scalaVersion match {
-      case "2.10" => Seq(pluginDependency)
-      case "2.11" => Seq(pluginDependency)
-      case "2.12" => Seq.empty
+      case VersionNumber((2 +: minor +: _), _, _) if minor < 12 => Seq(pluginDependency)
+      case VersionNumber((2 +: minor +: _), _, _) if minor >= 12 => Seq.empty
+      case VersionNumber((major +: _), _, _) if major > 2 => Seq.empty
     }
   }
 
   def scalaVersionToCompilerFlags(scalaVersion: String): Seq[String] = {
     scalaVersion match {
-      case "2.12" => Seq("-Ypartial-unification")
-      case _ => Seq.empty
+      case VersionNumber((2 +: minor +: _), _, _) if minor < 12 => Seq.empty
+      case VersionNumber((2 +: minor +: _), _, _) if minor >= 12 => Seq("-Ypartial-unification")
+      case VersionNumber((major +: _), _, _) if major > 2 => Seq("-Ypartial-unification")
     }
   }
 
   override val projectSettings = Seq(
     partialUnificationModule := "com.milessabin" % "si2712fix-plugin" % "1.2.0",
-    libraryDependencies ++= scalaVersionToDependencies(scalaBinaryVersion.value, partialUnificationModule.value),
+    libraryDependencies ++= scalaVersionToDependencies(scalaVersion.value, partialUnificationModule.value),
     scalacOptions ++= scalaVersionToCompilerFlags(scalaVersion.value)
   )
 }
