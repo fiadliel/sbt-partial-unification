a370b6c5daf93baa5a82bb67179be5cc805a8ad3
Daniel Spiewak <djspiewak@gmail.com>
2018-06-17 22:21:09 -0600

    (Hopefully) fixed 2.13 handling with the flag removal in M4

--- src/main/scala/PartialUnification.scala
+++ src/main/scala/PartialUnification.scala
@@ -13,24 +13,45 @@ object PartialUnification extends AutoPlugin {
 
   import autoImport._
 
-  def pluginOrFlag(scalaVersion: String, pluginModule: ModuleID): Either[ModuleID, String] = {
-    val pluginDependency = Left(compilerPlugin(pluginModule cross CrossVersion.full))
-    val flag = Right("-Ypartial-unification")
-    val VersionNumber((major +: minor +: patch +: _), _, _) = scalaVersion
+  def pluginOrFlag(scalaVersion: String, pluginModule: ModuleID): Resolution = {
+    val pluginDependency = Resolution.Plugin(compilerPlugin(pluginModule cross CrossVersion.full))
+    val flag = Resolution.Flag("-Ypartial-unification")
+    val VersionNumber((major +: minor +: patch +: _), tags, _) = scalaVersion
 
-    (major, minor, patch) match {
-      case (2, 10, p) if p >= 6 => pluginDependency
-      case (2, 11, 8) => pluginDependency
-      case (2, 11, p) if p >= 9 => flag
-      case (2, 12, _) => flag
-      case (2, 13, _) => flag
+    (major, minor, patch, tags) match {
+      case (2, 10, p, _) if p >= 6 => pluginDependency
+      case (2, 11, 8, _) => pluginDependency
+      case (2, 11, p, _) if p >= 9 => flag
+      case (2, 12, _, _) => flag
+      case (2, 13, _, milestone +: _) if milestone == "M1" || milestone == "M2" || milestone == "M3" => flag    // ignoring the M4 snapshot
+      case (2, 13, _, _) => Resolution.NothingHappens
       case _ => sys.error(s"scala version $scalaVersion is not supported by this plugin.")
     }
   }
 
   override val projectSettings = Seq(
     partialUnificationModule := "com.milessabin" % "si2712fix-plugin" % "1.2.0",
-    libraryDependencies ++= pluginOrFlag(scalaVersion.value, partialUnificationModule.value).left.toSeq,
-    scalacOptions ++= pluginOrFlag(scalaVersion.value, partialUnificationModule.value).right.toSeq
+
+    libraryDependencies ++= {
+      pluginOrFlag(scalaVersion.value, partialUnificationModule.value) match {
+        case Resolution.Plugin(id) => Seq(id)
+        case _ => Seq.empty
+      }
+    },
+
+    scalacOptions ++= {
+      pluginOrFlag(scalaVersion.value, partialUnificationModule.value) match {
+        case Resolution.Flag(flag) => Seq(flag)
+        case _ => Seq.empty
+      }
+    }
   )
+
+  sealed trait Resolution extends Product with Serializable
+
+  object Resolution {
+    final case class Plugin(id: ModuleID) extends Resolution
+    final case class Flag(flag: String) extends Resolution
+    case object NothingHappens extends Resolution
+  }
 }
